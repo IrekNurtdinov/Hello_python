b5d8a421ad0d36a2b1a33b1647292097a7ee3c9f
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2022-02-12 18:06:11 +0300

    on  ex 14

---
+++ Exs_for_sem_1-6/Ex_14.py
@@ -0,0 +1,16 @@
+# 14. Подсчитать сумму цифр в вещественном числе.
+
+a = 2.375
+
+b = str(a)
+sum = 0
+for i in b:
+    
+    if 0 < i < 9:
+        # d = int(b[i])
+        print(i)
+        # sum += int(b[i])
+    
+        
+# print(sum)
+
