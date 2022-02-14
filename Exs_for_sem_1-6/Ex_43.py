097d1fc86d3a0d708b376c55565977a356defdfe
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2022-02-14 15:56:10 +0300

    ex 43 solved

--- Exs_for_sem_1-6/Ex_43.py
+++ Exs_for_sem_1-6/Ex_43.py
@@ -1,14 +1,17 @@
 # Дана последовательность чисел. Получить список уникальных элементов заданной последовательности.
 # Пример: [1, 2, 3, 5, 1, 5, 3, 10] => [2, 10]
+# решено
+
+list = [1, 2, 3, 5, 1, 5, 3, 10,2,43,15]
+
+def unic_list(a):
+    new_list = []
+    for i in a:
+        if a.count(i) == 1:
+            new_list.append(i)
+    return new_list  
 
 
-list = [1, 2, 1, 5]
-new_list = []
-for i in range(0,len(list)-1):
-    for j in range(i+1,len(list)):
-        if list[i] == list[j]:
-            list.pop(i)
-            list.pop(j)
 
-print(list)   
+print(unic_list(list))
     
